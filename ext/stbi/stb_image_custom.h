9140418070da6b638e717dcde51611475e35055a
yuanxq <806516788@qq.com>
2025-06-03 15:23:08 +0800

    Update stb_image_custom.h
    我的锅！没看这代码

--- ext/stbi/stb_image_custom.h
+++ ext/stbi/stb_image_custom.h
@@ -4,7 +4,7 @@
 #define STBI_FAILURE_USERMSG
 
 #include "stb_image.h"
-#include "stb_image_resize.h"
+#include "stb_image_resize2.h"
 #include "stb_image_write.h"
 
 size_t stbi_write_jpg_memory(int width, int height, int comp, unsigned char* ptr, float quality, unsigned char** output);
